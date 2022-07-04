ed2b146d7174708403e2f6b9f5e7ddbde43628e9
Pannawat1122 <52130250+Pannawat1122@users.noreply.github.com>
2022-07-04 08:17:23 +0700

    db setting

--- conn.php
+++ conn.php
@@ -1,8 +1,8 @@
 <?php
-$host='localhost';
-$user='root';
-$password='';
-$database='shop';
+$host='bjgjhmvlfnm3fhlxmuip-mysql.services.clever-cloud.com';
+$user='uxxsvofv3k0zm1bg';
+$password='ZPTwiygr1ZFiwILrOCoq';
+$database='bjgjhmvlfnm3fhlxmuip';
  $con= mysqli_connect($host,$user,$password,$database);
  error_reporting(0);
 
